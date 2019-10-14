3d3b52a19f6b4698c020b9c5a05a6fbac9f1979d
pjehan <pierre.jehan@gmail.com>
2019-10-14 15:08:28 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# HTML
+
+## Installation
+
+```bash
+npm install -g revealexpress
+git clone https://github.com/pjehan-teaching/html
+cd html
+revealexpress
+```
---
+++ slideshow.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'CSS',
+  stylesheets: ['assets/font-awesome/css/font-awesome.min.css', 'assets/chartjs/Chart.min.css', 'assets/css/style.css'],
+  javascripts: ['assets/chartjs/Chart.bundle.min.js', 'assets/js/script.js']
+}
